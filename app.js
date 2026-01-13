1b5c37f9c276c9b0e97240a36c1e7a55e62b2477
Subrat Kumar Rout <subratrout986@gmail.com>
2026-01-14 02:35:39 +0530

    functionallity added

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ import ReactDOM from "react-dom/client";
 
 
 function Pass(){
-    let [password,setpassword] = useState("AAAjosdcpJXPj");
+    let [password,setpassword] = useState("");
     let [count,setcount] = useState(10);
     let [number,setnumber] = useState(false);
     let [char,setchar] = useState(false);
+
+
+    function updatepass(){
+        let chare = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        if(number){
+            chare += "1234567890"
+        }
+        if(char){
+            chare += "!@#$%^&*()+=~`"
+        }
+        
+        let res = "";
+
+        for(let i=0;i<count;i++){
+            res += chare[Math.floor(Math.random()*chare.length)]
+        }
+
+        setpassword(res);
+    }
+
+    
+    useEffect(()=>
+        updatepass()
+        ,[count,number,char])
 
 
 
@@ -15,13 +39,13 @@ function Pass(){
             <h1>{password}</h1>
 
             <div>
-                <input type="range" min={5} max={50}></input>
+                <input type="range" min={5} max={20} onChange={(e)=>setcount(e.target.value)}></input>
                 <label>Range:{count}</label>
 
-                <input type="radio" defaultValue={number} ></input>
+                <input type="checkbox" defaultChecked={number} onChange={()=>setnumber(!number)}></input>
                 <label>Number</label>
 
-                <input type="radio" defaultValue={char} ></input>
+                <input type="checkbox" defaultChecked={char} onChange={()=>setchar(!char)}></input>
                 <label>Character</label>
             </div>
         </>
