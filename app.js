ece8b06181b629c715afd2448e8a4c686d449f2a
Subrat Kumar Rout <subratrout986@gmail.com>
2026-01-14 01:41:09 +0530

    added UI

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 import React,{useState,useEffect} from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 
-function pass(){
-    let [password,setpassword] = useState("");
+function Pass(){
+    let [password,setpassword] = useState("AAAjosdcpJXPj");
     let [count,setcount] = useState(10);
-    let [number,setnumber] = useState("false");
-    let [char,setchar] = useState("false");
+    let [number,setnumber] = useState(false);
+    let [char,setchar] = useState(false);
 
 
 
     return(
-        <div>
+        <>
             <h1>{password}</h1>
 
             <div>
-                
+                <input type="range" min={5} max={50}></input>
+                <label>Range:{count}</label>
+
+                <input type="radio" defaultValue={number} ></input>
+                <label>Number</label>
+
+                <input type="radio" defaultValue={char} ></input>
+                <label>Character</label>
             </div>
-        </div>
+        </>
         
 
     );
@@ -27,4 +34,7 @@ function pass(){
 }
 
 
-ReactDOM.craeteRoot(document.getElementById("main")).render(pass);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Pass/>);
+// ReactDOM.createRoot(document.getElementById("main")).render(<pass/>);
